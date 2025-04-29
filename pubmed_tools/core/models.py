bd5d80eadf1b8af228a9a530dcc33a23e94f9591
Tanuj <Tanuj@cmu.edu>
2025-04-29 11:02:51 +0530

    Reorder fields for models.py
    - This affects column order in generated csv files

--- pubmed_tools/core/models.py
+++ pubmed_tools/core/models.py
@@ -6,17 +6,17 @@ class ArticleDetails(TypedDict):
 
     Fields:
         title: Article title as string
-        abstract: Full abstract text as string
-        authors: List of author names as strings
         publication_date: Defaults to dict with keys 'year', 'month', 'day'.
                            If convert_date=True, value is a string in the format YYYY-MM-DD
         pmid: PubMed ID as string
+        authors: List of author names as strings
+        abstract: Full abstract text as string
     """
     title: str
-    abstract: str
-    authors: List[str]
     publication_date: Union[str, dict]
     pmid: str
+    authors: List[str]
+    abstract: str
 
 
 class SearchResult(TypedDict):
